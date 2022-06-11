4620e947577f1552dc18d5fa2cdf5c28b1ef1f86
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2022-06-10 22:06:31 -0400

    Create cobblestoneMiner.lua

---
+++ src/Mining/cobblestoneMiner.lua
@@ -0,0 +1,12 @@
+function mineGeneratedCobble()
+    local sleepTime = 1000
+
+
+    while()
+        turtle.dig()
+        os.sleep(sleepTime)
+        print('Fuel Level' .. turtle.getFuelLevel()])
+    end
+end
+
+mineGeneratedCobble()

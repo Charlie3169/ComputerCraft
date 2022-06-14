00bc3a4a17ef4c4e0624ab89e21c6b7263cc7d18
YummyYummyHippo <YummyYummyHippos@gmail.com>
2022-06-14 11:58:02 -0400

    Adding a test function that has all things that might need to be defined when a turtle is initialized

---
+++ test/test.lua
@@ -0,0 +1,16 @@
+package.path = package.path .. ";../lib/mining.lua"
+require('mining')
+
+UNLOADING_STATION_COORDS = {-657,119,731,3}
+REFUELING_STATION_COORDS = {-657,119,731,1}
+
+currentX = -653
+currentY = 120
+currentZ = 736
+currentDirectionIndex = 3
+COORDINATES_TRACKED = true
+
+targetDig = {-653,119,731}
+
+moveTo(targetDig[1],targetDig[2],targetDig[3])
+digArea(2,-1,2,-1)
